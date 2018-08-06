6869979cabb943b32e81e8b6bf4bbde8167bc3ec
syui <syui@syui.cf>
2018-08-06 10:58:56 +0900

    fix

--- bin/meta.zsh
+++ bin/meta.zsh
@@ -4,21 +4,27 @@ set -eu
 f=${0:a:h}/FFMETADATAFILE
 o=${0:a:h}/out.mp3
 dir=`ls ${0:a:h}`
+
 file=`cat << EOF | peco
 ${dir}
 EOF` > /dev/null 2>&1
 
+n=`echo $dir|wc -l|tr -d ' '`
+n=`expr $n - 2`
+echo $n
+file_name=${file%%.*}
+echo $file_name
 echo ";FFMETADATA1
-title=
-title-sort=
+title=$file_name
+title-sort=$file_name
 artist=syui
 album=origin/master
 album_artist=$USER
 comment=https://syui.cf @${USER}@${USER}.cf
 composer=git@github.com:${USER}/vocaloid
-disc=1/x
+disc=1/$n
 genre=vocaloid
-track=1/x
+track=1/$n
 TYER=`date '+%Y-%m-%d'`
 artist-sort=$USER
 encoder=$USER" >! $f
